2c1fbe980ca5432d456cd7fa8db248ec5a36afe9
Georgy Shkuratov <g.shkuratov@carprice.ru>
2018-07-18 10:25:43 +0300

    добавлена поддержка order book

--- candles-gen.js
+++ candles-gen.js
@@ -1,11 +1,38 @@
 var { A, F } = require('./readers/constants')
+var { showFlags } = require('./readers/utils')
 
 function candlesGenerator(filename, timeframe) {
   var i = 0
+  let ordBook = {}
   return function(data, flags, operFlags) {
     i++
-    if(i%100000 === 0) {
-      console.log('i:',i)
+    const { price, volume } = data;
+    const isAdd = (operFlags & F.Add) != 0;
+    const isBuy = (operFlags & F.Buy) != 0;
+    const isSell = (operFlags & F.Sell) != 0;
+    if (operFlags & F.FlowStart) {
+      ordBook = {}
+    }
+
+    if(!(isBuy ^ isSell) || operFlags & F.NonSystem || operFlags & F.NonZeroReplAct) {
+      console.log('strange order:', showFlags(F, operFlags), showFlags(A, flags));
+      return;
+    }
+    let quantity = ordBook[price] || 0
+    if ((isAdd && isSell) || (!isAdd && isBuy)) {
+       quantity += volume
+    } else {
+       quantity -= volume
+    }
+     
+    if (quantity === 0) {
+      delete ordBook[price]
+    } else {
+      ordBook[price] = quantity
+    }
+
+    if (i%10000 === 0) {
+      console.log('i:',i, Object.keys(ordBook).length)
     }
   }
 }
